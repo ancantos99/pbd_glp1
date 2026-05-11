b04cf64694e61aab1d5c266fb8b588d022d0b341
Andres Cantos <andres.cantos@gizlocorp.com>
2026-05-11 11:07:22 -0500

    editar variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   adminEmail: 'acantos@netvoice.ec',
   emailjs: {
-    serviceId: 'YOUR_EMAILJS_SERVICE_ID',
-    templateId: 'YOUR_EMAILJS_TEMPLATE_ID',
-    publicKey: 'YOUR_EMAILJS_PUBLIC_KEY',
+    serviceId: 'service_h0hpega',
+    templateId: 'template_v116wdw',
+    publicKey: 'orhWAE-R8yGxN24n5',
   },
 };
